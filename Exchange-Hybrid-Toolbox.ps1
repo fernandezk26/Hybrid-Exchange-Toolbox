3419551550ac365da9c1e416e39aa5cad35b1420
Kevin Fernandez <33888011+fernandezk26@users.noreply.github.com>
2020-06-23 15:32:39 -0500

    Update Exchange-Hybrid-Toolbox.ps1
    beautified

--- Exchange-Hybrid-Toolbox.ps1
+++ Exchange-Hybrid-Toolbox.ps1
@@ -1,4 +1,4 @@
-﻿#this script has functions for the most common exchange tasks I have to perform at work. These are things that usually take much longer using the GUI
+#this script has functions for the most common exchange tasks I have to perform at work. These are things that usually take much longer using the GUI
 #You will have to edit it just a bit to fit your needs, this ranges from entering your connection URI's, and editing new user OU, name formatting, and 
 #licensing info to match your company.
 
@@ -21,148 +21,140 @@ Connect-MsolService -Credential $cred
 #to work in powershell ISE. Regular powershell closes/stops after Connect-Msolservice. 
 function Run-Toolbox {
 
-function Create-User
-{
-    #Populate Variables used in the new user creation, you will have to edit some of this for your own company needs. We use this naming convention as do many
-    #companies I have worked at, but yours may be different. For the OU, we use time zones, etc. Yours may be different.
-     $UserName = Read-Host 'Please enter the first name in the following format: Bob Joe'
-     $Password = Read-Host 'Please enter the temporary password' -AsSecureString
-     $FirstName,$LastName = $UserName.split(' ')
-     $DisplayName = $lastName + ', ' + $firstName
-     $SamAccountName = $firstName + '.' + $lastName
-     $domain = '@yourdomain.com'
-     $Email = $FirstName + '.' + $LastName + $domain
-     $title = Read-Host "Please enter user Title"
-     $branch = Read-Host "Please enter the branch number here"
-     $Description = $Branch + ' / ' + $title
-     $OU = Get-ADOrganizationalUnit -Filter 'Name -like $branch' | select distinguishedName -ExpandProperty distinguishedname
+	function Create-User
+	{
+		#Populate Variables used in the new user creation, you will have to edit some of this for your own company needs. We use this naming convention as do many
+		#companies I have worked at, but yours may be different. For the OU, we use time zones, etc. Yours may be different.
+		$UserName = Read-Host 'Please enter the first name in the following format: Bob Joe'
+		$Password = Read-Host 'Please enter the temporary password' -AsSecureString
+		$FirstName,$LastName = $UserName.split(' ')
+		$DisplayName = $lastName + ', ' + $firstName
+		$SamAccountName = $firstName + '.' + $lastName
+		$domain = '@yourdomain.com'
+		$Email = $FirstName + '.' + $LastName + $domain
+		$title = Read-Host "Please enter user Title"
+		$branch = Read-Host "Please enter the branch number here"
+		$Description = $Branch + ' / ' + $title
+		$OU = Get-ADOrganizationalUnit -Filter 'Name -like $branch' | Select-Object distinguishedName -ExpandProperty distinguishedname
 
-    #Creates User AD account and O365 Mailbox On prem first, which later syncs to O365
-    New-RemoteMailbox -Name $DisplayName -FirstName $FirstName -LastName $LastName -DisplayName $DisplayName -SamAccountName $SamAccountName -Confirm:$false -PrimarySmtpAddress $Email -Password $Password -UserPrincipalName $Email -ResetPasswordOnNextLogon $true
-    
-    #There are sometimes delays from our sync from Exchange on prem to AD, so I put a 30 second timer just in case.
-    Start-Sleep -Seconds 30
+		#Creates User AD account and O365 Mailbox On prem first, which later syncs to O365
+		New-RemoteMailbox -Name $DisplayName -FirstName $FirstName -LastName $LastName -DisplayName $DisplayName -SamAccountName $SamAccountName -Confirm:$false -PrimarySmtpAddress $Email -Password $Password -UserPrincipalName $Email -ResetPasswordOnNextLogon $true
 
-    #manipulate AD object after allowing 30 seconds to sync, how long it typically takes our environment to sync. Your replication may be faster.
-    #We have core groups, but you could also make this "smarter" by using If statements to apply groups based on title/description.
-    Add-ADGroupMember -identity Group1 -Members $SamAccountName
-    Add-ADGroupMember -identity "Group 3" -Members $SamAccountName
-    Set-ADUser -Identity $SamAccountName -Description $description -Title $title 
+		#There are sometimes delays from our sync from Exchange on prem to AD, so I put a 30 second timer just in case.
+		Start-Sleep -Seconds 30
 
-    #Move to OU, first get the Root OU from user, then specify the target OU. Our OU's have a "Users" sub folder so I added that to the $TargetOU
-     $CN = get-aduser -identity $samAccountName -properties DistinguishedName | Select distinguishedname -expandproperty distinguishedName
-     $TargetOU = Get-ADOrganizationalUnit -Filter 'Name -like $branch' | select distinguishedname -ExpandProperty distinguishedname 
-     $TargetOU = "OU=Users," + $TargetOU
-     Move-ADObject -Identity $CN -TargetPath $TargetOU
-}
+		#manipulate AD object after allowing 30 seconds to sync, how long it typically takes our environment to sync. Your replication may be faster.
+		#We have core groups, but you could also make this "smarter" by using If statements to apply groups based on title/description.
+		Add-ADGroupMember -Identity Group1 -Members $SamAccountName
+		Add-ADGroupMember -Identity "Group 3" -Members $SamAccountName
+		Set-ADUser -Identity $SamAccountName -Description $description -Title $title
 
-function Enable-OOO {
-$identity = Read-Host "Enter the username of the person you would like to turn OOO on for" 
-$message = Read-Host "Please enter the message you would like to add to the mailbox"
-Set-MailboxAutoReplyConfiguration -Identity $identity -AutoReplyState Enabled -InternalMessage "$message" -ExternalMessage "$message"
-}
+		#Move to OU, first get the Root OU from user, then specify the target OU. Our OU's have a "Users" sub folder so I added that to the $TargetOU
+		$CN = Get-ADUser -Identity $samAccountName -properties DistinguishedName | Select-Object distinguishedname -ExpandProperty distinguishedName
+		$TargetOU = Get-ADOrganizationalUnit -Filter 'Name -like $branch' | Select-Object distinguishedname -ExpandProperty distinguishedname
+		$TargetOU = "OU=Users," + $TargetOU
+		Move-ADObject -Identity $CN -TargetPath $TargetOU
+	}
 
-function Disable-OOO {
-$identity = Read-Host "Enter the username of the person you would like to turn OOO off for" 
-Set-MailboxAutoReplyConfiguration -Identity $identity -AutoReplyState Disabled
-}
+	function Enable-OOO {
+		$identity = Read-Host "Enter the username of the person you would like to turn OOO on for"
+		$message = Read-Host "Please enter the message you would like to add to the mailbox"
+		Set-MailboxAutoReplyConfiguration -Identity $identity -AutoReplyState Enabled -InternalMessage "$message" -ExternalMessage "$message"
+	}
 
-#You will have to get your own license SKU's by running the Get-MsolAccountSku command. From there you can specify which licenses you would like to remove
-#This applies to the EnableUserAccess and Disable-UserAccessfunctions
+	function Disable-OOO {
+		$identity = Read-Host "Enter the username of the person you would like to turn OOO off for"
+		Set-MailboxAutoReplyConfiguration -Identity $identity -AutoReplyState Disabled
+	}
 
-#At our organization, admins disable user access by disabling the AD account, convert the mailbox to shared, and remove any groups/licenses
-#We laid off people due to Covid, so we had to eventually enable their accounts again. This function makes that easy (instead of going to 3 places)
-function ReEnable-UserAccess{
-$username = Read-Host "Enter username of the person you would like to re-enable"
-Enable-ADAccount -Identity $username
+	#You will have to get your own license SKU's by running the Get-MsolAccountSku command. From there you can specify which licenses you would like to remove
+	#This applies to the EnableUserAccess and Disable-UserAccessfunctions
 
-#older versions of exchange have an issue where the RemoteRecipientType changes to "Migrated" when converted to shared. This manually fixes that in the AD object.
-set-aduser $username -replace @{msExchRemoteRecipientTYpe="1"} 
-#recipient type of 1 = ProvisionMailbox
+	#At our organization, admins disable user access by disabling the AD account, convert the mailbox to shared, and remove any groups/licenses
+	#We laid off people due to Covid, so we had to eventually enable their accounts again. This function makes that easy (instead of going to 3 places)
+	function ReEnable-UserAccess {
+		$username = Read-Host "Enter username of the person you would like to re-enable"
+		Enable-ADAccount -Identity $username
 
-#Change this to match your groups that were removed if applicable
-Add-ADGroupMember -Identity Group1 -Members $username
-Add-ADGroupMember -Identity "Group 2" -Members $username
-Start-sleep -seconds 15
- 
-#Convert back to normal mailbox
-Set-RemoteMailbox -Identity $username -Type regular
-Set-MailboxAutoReplyConfiguration -Identity $username -AutoReplyState Disabled
+		#older versions of exchange have an issue where the RemoteRecipientType changes to "Migrated" when converted to shared. This manually fixes that in the AD object.
+		#recipient type of 1 = ProvisionMailbox
+		Set-ADUser $username -Replace @{ msExchRemoteRecipientTYpe = "1" }
 
-#adding default 0365 license back
-Set-MsolUserLicense -UserPrincipalName "$identity@nsm-seating.com" -AddLicenses "yourdomain:ENTERPRISEPACK"
 
-#adding additional mobile license if title matches
- $title = get-aduser -identity $username -properties title | select title -expandproperty title
- if($title -match "ATP" -or $title -match "RTS") {
- Set-MsolUserLicense -UserPrincipalName "$username@nsm-seating.com" -AddLicenses "nsmseating:ENTERPRISE_MOBILE"
- }
-}
+		#Change this to match your groups that were removed if applicable
+		Add-ADGroupMember -Identity Group1 -Members $username
+		Add-ADGroupMember -Identity "Group 2" -Members $username
+		Start-Sleep -Seconds 15
 
-#I just made this quick function just in case I needed it. usually user access is disabled far before it gets to me.
-function Disable-UserAccess {
-$identity = Read-Host "Enter the username of the persons mailbox you would like to convert to shared"
-Set-RemoteMailbox -Identity $identity -Type shared
-Set-MsolUserLicense -UserPrincipalName "$identity@yourdomain.com" -RemoveLicenses "<Your License SKU here>"
-}
- 
-function Approve-MobileDevice {
-$identity = Read-Host "Enter username, such as bob.joe"
-Get-MobileDevice -Mailbox $identity | fl FriendlyName, Identity, DeviceAccessState, DeviceID 
-"Copy the the DeviceId of the quarantined phone for the next part"
-$deviceID = Read-host "Copy the the DeviceId of the quarantined phone and paste DeviceID here"
-Set-CASMailbox -identity $identity -ActiveSyncAllowedDeviceIDs @{add= $deviceID}
-}
+		#Convert back to normal mailbox
+		Set-RemoteMailbox -Identity $username -Type regular
 
-function Delegate-Mailbox {
-$identity = Read-Host "Enter the username of the user whos mailbox you want to delegate, in the firstName.lastName format"
-$trustee = Read-Host "Enter the username of the trustee, firstName.lastName (person who is getting access)"
-Add-MailboxPermission -Identity $identity -User $trustee -AccessRights FullAccess
-}
+		#Turn off Out of office
+		Set-MailboxAutoReplyConfiguration -Identity $username -AutoReplyState Disabled
 
-function Remove-DelegatedMailbox {
-$identity = Read-Host "Enter the username of the user whos mailbox you want to remove permissions from, in the firstName.lastName format"
-$trustee = Read-Host "Enter the username of the person you want removed, firstName.lastName "
-Remove-MailboxPermission -Identity $identity -User $trustee -AccessRights FullAccess
-}
+		#adding default 0365 license back
+		Set-MsolUserLicense -UserPrincipalName "$identity@yourdomain.com" -AddLicenses "yourdomain:ENTERPRISEPACK"
 
-Switch(Read-Host 'Select from the following options: 
+		#adding additional mobile license if title matches
+		$title = Get-ADUser -Identity $username -properties title | Select-Object title -ExpandProperty title
+		if ($title -match "Mobile" -or $title -match "Technician") {
+			Set-MsolUserLicense -UserPrincipalName "$username@yourdomain.com" -AddLicenses "nsmseating:ENTERPRISE_MOBILE"
+		}
+	}
+
+	#I just made this quick function just in case I needed it. usually user access is disabled far before it gets to me.
+	function Disable-UserAccess {
+		$identity = Read-Host "Enter the username of the persons mailbox you would like to convert to shared"
+		Set-RemoteMailbox -Identity $identity -Type shared
+		Set-MsolUserLicense -UserPrincipalName "$identity@yourdomain.com" -RemoveLicenses "<Your License SKU here>"
+	}
+
+	function Approve-MobileDevice {
+		$identity = Read-Host "Enter username, such as bob.joe"
+		Get-MobileDevice -Mailbox $identity | Format-List FriendlyName,Identity,DeviceAccessState,DeviceID
+		"Copy the the DeviceId of the quarantined phone for the next part"
+		$deviceID = Read-Host "Copy the the DeviceId of the quarantined phone and paste DeviceID here"
+		Set-CASMailbox -Identity $identity -ActiveSyncAllowedDeviceIDs @{ Add = $deviceID }
+	}
+
+	function Delegate-Mailbox {
+		$identity = Read-Host "Enter the username of the user whos mailbox you want to delegate, in the firstName.lastName format"
+		$trustee = Read-Host "Enter the username of the trustee, firstName.lastName (person who is getting access)"
+		Add-MailboxPermission -Identity $identity -User $trustee -AccessRights FullAccess
+	}
+
+	function Remove-DelegatedMailbox {
+		$identity = Read-Host "Enter the username of the user whos mailbox you want to remove permissions from, in the firstName.lastName format"
+		$trustee = Read-Host "Enter the username of the person you want removed, firstName.lastName "
+		Remove-MailboxPermission -Identity $identity -User $trustee -AccessRights FullAccess
+	}
+
+	switch (Read-Host 'Select from the following options: 
 _________________________________________________________________________________________________
-
        1. Create a New User (on Prem, syncs to O365)
-
        2. Enable OOO for a user
         
        3. Disable OOO for a user
-
        4. Disable user access (convert mailbox to shared and remove E3 license)
-
        5. Enable user access (Convert mailbox to regular and assign E3 license)
-
        6. Approve a mobile device
-
        7. Delegate mailbox
-
        8. Remove delegated mailbox permissions
-
        9. Exit
 _________________________________________________________________________________________________ 
       
        Selection
        ===>') {
 
-   1{Create-User}
-   2{Enable-OOO}
-   3{Disable-OOO}
-   4{Disable-UserAccess}
-   5{ReEnable-UserAccess}
-   6{Approve-MobileDevice}
-   7{Delegate-Mailbox}
-   8{Remove-DelegatedMailbox}
-   9{break -noexit}
+		1 { Create-User }
+		2 { Enable-OOO }
+		3 { Disable-OOO }
+		4 { Disable-UserAccess }
+		5 { ReEnable-UserAccess }
+		6 { Approve-MobileDevice }
+		7 { Delegate-Mailbox }
+		8 { Remove-DelegatedMailbox }
+		9 { break -noexit }
+	}
 }
-}
-
-
-
 
